6adca5d2ad388997a0a9613af498b0afb5f70c9d
Andrew Walker <hovercar00@gmail.com>
2018-01-09 17:26:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return(`It's ${days} daysuntil ${holiday}!`);
+  return(`It's ${days} days until ${holiday}!`);
 }
